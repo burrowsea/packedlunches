457b10c1a3c9109db32ca97b93ac78bc4c9d392e
burrows.ea <burrows.ea@oundleschool.org.uk>
2025-06-03 10:32:46 +0100

    making install file

--- install.php
+++ install.php
@@ -1,2 +1,26 @@
 <?php
 include_once("connection.php");
+
+##Create Users Table
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS tblusers;
+CREATE TABLE tblusers 
+(userid INT(8) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+role TINYINT(1),
+username VARCHAR(50) NOT NULL,
+email VARCHAR(70) NOT NULL,
+password VARCHAR(100) NOT NULL,
+surname VARCHAR(50) NOT NULL,
+forename VARCHAR(100) NOT NULL,
+house VARCHAR(20) NOT NULL)"
+);
+$stmt->execute();
+$stmt->closeCursor();
+echo("tblusers created");
+
+##Create Table for Items
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS tblitems;
+CREATE TABLE tblitems
+(itemid INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name
